1f67ab92603cab0f899dc48cb3b9904e0986aa51
Melton <meltontr@appd.appstate.edu>
2017-03-16 06:14:24 -0400

    Add project page

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-
 app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
+
     console.log(log);
     fs.appendFile('server.log', log + '\n');
     next();
 });
 
-app.use((req, res, next) => {
-   res.render('maintenance.hbs')
-});
+//app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+//});
 
 app.use(express.static(__dirname + '/public')); 
 
@@ -41,6 +41,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
     });
 });
 
